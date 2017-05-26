46c6fee67f5d34b0839627a36e829f6e08e0e9a6
danylo <fedorough@gmail.com>
2017-05-26 19:14:22 +0300

    fixed some

--- debug-print.lisp
+++ debug-print.lisp
@@ -7,16 +7,18 @@
   (labels ((%apply-autistic-formatting (list)
              (let ((prefix (format nil "~4A " (format nil "~A>" *dbp-count*))))
                (cons prefix (mapcar (lambda (el) (if (eq el :nl) (format nil "~%~A" prefix) (format nil "~A " el))) list)))))
-    (when increase-count? (incf *dbp-count*))
-    (format stream "~@[~{~A~}~]~%" (%apply-autistic-formatting output-list))))
+    (format stream "~@[~{~A~}~]~%" (%apply-autistic-formatting output-list))
+    (when increase-count? (incf *dbp-count*))))
 
 (defun dbp (&rest output-list)
   "simple"
   (dbpe :stream t :increase-count? t :output-list output-list))
 
-(defun dbps (&rest output-list-list)
-  "several"
-  (dbp (apply #'append output-list-list)))
+(defmacro dbps (&rest output-list-list)
+  "several with implicit :nl"
+  `(progn 
+     ,@(mapcar (lambda (output-list) `(dbpe :stream t :increase-count? nil :output-list ',output-list)) (butlast output-list-list))
+     (dbpe :stream t :increase-count? t :output-list ',(car (last output-list-list)))))
 
 (defun dbp-reset ()
   (setf *dbp-count* 0))
