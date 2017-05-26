d409bf8d27dc6aae78ed621ea5281fdb83afd12f
danylo <fedorough@gmail.com>
2017-05-26 20:22:34 +0300

    formatting

--- debug-print.lisp
+++ debug-print.lisp
@@ -4,12 +4,18 @@
 
 (defun dbpe (&key (stream t) (increase-count? t) (place-for-count 4) (trailing-newline? t) (delimiter " ") (add-prefixes? t) output-list)
   "extended"
-  (let* ((prefix (format nil (format nil "~~~AA " place-for-count) (format nil "~A>" *dbp-count*)))
-         (prefix-chars (coerce prefix 'list)))
+  (let* ((prefix (format nil
+                         (format nil "~~~AA " place-for-count)
+                         (format nil "~A>" *dbp-count*))))
     (labels ((%apply-autistic-formatting (list)
                (mapcar (lambda (el) (if (eq el :nl) (format nil "~%") (format nil "~A~A" el delimiter))) list))
              (%intercept-newlines-with-prefix (string)
-               (coerce (loop for c across string appending (if (eq c #\newline) (cons #\newline prefix-chars) (list c))) 'string)))
+               (coerce
+                (loop for c across string
+                      appending (if (eq c #\newline)
+                                    (cons #\newline (coerce prefix 'list))
+                                    (list c)))
+                'string)))
       (let ((str (format nil "~@[~{~A~}~]" (%apply-autistic-formatting output-list))))
         (format stream "~A~:[~;~%~]" (format nil "~A~A" prefix (if add-prefixes? (%intercept-newlines-with-prefix str) str)) trailing-newline?)
         (when increase-count? (incf *dbp-count*))))))
