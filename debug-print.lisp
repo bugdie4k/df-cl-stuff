47fc7073e79f11fb0a76fcf8efce46fa3edd6216
danylo <fedorough@gmail.com>
2017-05-26 20:11:38 +0300

    improvements

--- debug-print.lisp
+++ debug-print.lisp
@@ -2,23 +2,27 @@
 
 (defvar *dbp-count* 0)
 
-(defun dbpe (&key (stream t) (increase-count? t) output-list)
+(defun dbpe (&key (stream t) (increase-count? t) (place-for-count 4) (trailing-newline? t) (delimiter " ") (add-prefixes? t) output-list)
   "extended"
-  (labels ((%apply-autistic-formatting (list)
-             (let ((prefix (format nil "~4A " (format nil "~A>" *dbp-count*))))
-               (cons prefix (mapcar (lambda (el) (if (eq el :nl) (format nil "~%~A" prefix) (format nil "~A " el))) list)))))
-    (format stream "~@[~{~A~}~]~%" (%apply-autistic-formatting output-list))
-    (when increase-count? (incf *dbp-count*))))
+  (let* ((prefix (format nil (format nil "~~~AA " place-for-count) (format nil "~A>" *dbp-count*)))
+         (prefix-chars (coerce prefix 'list)))
+    (labels ((%apply-autistic-formatting (list)
+               (mapcar (lambda (el) (if (eq el :nl) (format nil "~%") (format nil "~A~A" el delimiter))) list))
+             (%intercept-newlines-with-prefix (string)
+               (coerce (loop for c across string appending (if (eq c #\newline) (cons #\newline prefix-chars) (list c))) 'string)))
+      (let ((str (format nil "~@[~{~A~}~]" (%apply-autistic-formatting output-list))))
+        (format stream "~A~:[~;~%~]" (format nil "~A~A" prefix (if add-prefixes? (%intercept-newlines-with-prefix str) str)) trailing-newline?)
+        (when increase-count? (incf *dbp-count*))))))
 
 (defun dbp (&rest output-list)
   "simple"
-  (dbpe :stream t :increase-count? t :output-list output-list))
+  (dbpe :stream t :increase-count? t :place-for-count 4 :trailing-newline? t :delimiter " " :add-prefixes? t :output-list output-list))
 
 (defmacro dbps (&rest output-list-list)
   "several with implicit :nl"
   `(progn 
-     ,@(mapcar (lambda (output-list) `(dbpe :stream t :increase-count? nil :output-list ',output-list)) (butlast output-list-list))
-     (dbpe :stream t :increase-count? t :output-list ',(car (last output-list-list)))))
+     ,@(mapcar (lambda (output-list) `(dbpe :increase-count? nil :output-list ',output-list)) (butlast output-list-list))
+     (dbpe :increase-count? t :output-list ',(car (last output-list-list)))))
 
 (defun dbp-reset ()
   (setf *dbp-count* 0))
