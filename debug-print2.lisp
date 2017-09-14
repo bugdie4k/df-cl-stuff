e5e100c934b5f8d4573f2dccd8d22d60360adfd6
bugdie4k <fedorough@gmail.com>
2017-09-14 23:57:29 +0300

    wip

--- debug-print2.lisp
+++ debug-print2.lisp
@@ -1,5 +1,7 @@
 (in-package #:df-cl-utils)
 
+;; utils
+
 (defmacro defclass* (name direct-superclasses &rest direct-slots)
   `(defclass ,name ,direct-superclasses
      ,(when direct-slots
@@ -16,10 +18,42 @@ all the result list to a single list. FUNCTION must return a list."
   (loop for results in (apply #'mapcar function lists)
         append results))
 
+(defmacro format* (fmt-or-string size &key (format-letter :A) align truncate?)
+  `(format*-aux ,(if (and (listp fmt-or-string) (stringp (first fmt-or-string))) `(format nil ,@fmt-or-string) fmt-or-string) ,size :format-letter ,format-letter :align ,align :truncate? ,truncate?))
+
+(defun format*-aux (string size &key format-letter align truncate?)
+  (labels ((%wut-align ()
+             (cond ((or (eq align :right) (eq align :r)) t)
+                   ((or (eq align :left) (eq align :l)) nil)
+                   (t nil)))
+           (%truncate-string-maybe ()
+             (if (and truncate? (> (length string) size)) (subseq string 0 size) string)))
+    (format nil
+            (format nil "~~~A~:[~;@~]~A" size (%wut-align) format-letter)
+            (%truncate-string-maybe))))
+
+;; counter
+
+(defvar *dbp-counter* 0)
+
+(defun count++ ()
+  (incf *dbp-counter*))
+
+(defun dbp-reset-counter ()
+  (setf *dbp-counter* 0))
+
 ;; format
 
 (defclass* fmt-size-mixin ()
   (size 1))
+
+(defclass* fmt-braces-mixin ()
+  (brace-left "")
+  (brace-right ""))
+
+(defmethod apply-braces ((braces fmt-braces-mixin) str &key (format-string "~A~A~A"))
+  (with-slots (brace-left brace-right) braces
+    (format nil format-string brace-left str brace-right)))
 
 (defclass* fmt-clip (fmt-size-mixin)
   (size 1)
@@ -28,10 +62,6 @@ all the result list to a single list. FUNCTION must return a list."
   (horizontal "─")
   (vertical "│")
   (side :left))
-
-(defclass* fmt-braces-mixin ()
-  (brace-left "")
-  (brace-right ""))
 
 (defclass* fmt-counter (fmt-size-mixin fmt-braces-mixin)
   (size 4)
@@ -50,6 +80,30 @@ all the result list to a single list. FUNCTION must return a list."
   (msg (make-instance 'fmt-msg))
   (order '(:clip :counter :prefix :msg)))
 
+(defmethod prepare-clip ((clip fmt-clip) place)
+  (with-slots (up down vertical horizontal size side brace-left brace-right) clip
+    (labels ((%build-corner (corner-char)
+               (let* ((corner-char (format* corner-char 1 :truncate? t))
+                      (rest (format* horizontal (1- size) :truncate? t))
+                      (left-part (if (eq side :left) corner-char rest))
+                      (right-part (if (eq side :left) rest corner-char)))
+                 (format nil "~A~A" left-part right-part))))
+      (case place
+        (:upper (%build-corner up))
+        (:middle (format* vertical size :align side :truncate? t))
+        (:lower (%build-corner down))))))
+
+(defmethod prepare-counter ((counter fmt-counter))
+  (with-slots (size brace-left brace-right) counter
+    (format* (apply-braces counter *dbp-counter*) size)))
+
+(defmethod prepare-prefix ((prefix fmt-prefix) str)
+  (with-slots (size brace-left brace-right) prefix
+    (format* (apply-braces prefix str) size :truncate? t)))
+
+(defmethod prepare-msg ((msg fmt-msg) str)
+  )
+
 ;; EXAMPLE:
 
 ;; (build-fmt
@@ -58,7 +112,7 @@ all the result list to a single list. FUNCTION must return a list."
 ;;  (:prefix :size 16 :brace-left "" :brace-right "")
 ;;  (:msg :size 60 :brace-left "" :brace-right "")
 ;;  (:order :clip :counter :prefix :msg))
-(defmacro build-fmt (&rest clauses)
+(defun build-fmt (&rest clauses)
   (labels ((%parse-fmt-clauses (clauses)
              (mappend (lambda (clause)
                        (let ((type (first clause))
@@ -66,16 +120,25 @@ all the result list to a single list. FUNCTION must return a list."
                          (if (eq type :order)
                              `(:order ',initargs)
                              (list type `(make-instance ',(intern (format nil "FMT-~A" (symbol-name type)) :df-cl-utils) ,@initargs)))))
-                     clauses)))
-    `(make-instance 'fmt ,@(%parse-fmt-clauses clauses))))
+                      clauses)))
+    (apply #'make-instance 'fmt (%parse-fmt-clauses clauses))))
 
 ;; options
 
-(defmacro build-options (&rest clauses)
-  ())
+(defclass* options ()
+  (out t) ; output stream
+  (w-delim " ") ; thing that would delimit tokens
+  (nl-before-line-delim t) ; newline before line delimiter
+  (nl-after-line-delim t) ; newline after line delimiter
+  (macros nil))
 
-(defun dbp-extended (&key (msg (error "DBPE NEEDS OUTPUT-LIST ARGUMENT")) (fmt nil) (options nil))
-  )
+(defun build-options (&rest clauses)
+  (apply #'make-instance 'options clauses))
+
+(defun dbp-extended (&key (opts nil) (msg "") (fmt nil))
+  (let ((opts (build-options opts))
+        (fmt (build-fmt fmt)))
+    ()))
 
 (defmacro dbp ())
 
