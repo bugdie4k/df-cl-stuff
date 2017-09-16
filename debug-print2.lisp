ade64931d697baa1f589132eee629eb4bd5af5bb
bugdie4k <fedorough@gmail.com>
2017-09-16 18:31:41 +0300

    baaad

--- debug-print2.lisp
+++ debug-print2.lisp
@@ -78,7 +78,43 @@ all the result list to a single list. FUNCTION must return a list."
   (counter (make-instance 'fmt-counter))
   (prefix (make-instance 'fmt-prefix))
   (msg (make-instance 'fmt-msg))
-  (order '(:clip :counter :prefix :msg)))
+  (order '(:clip :counter :msg)))
+
+;; EXAMPLE:
+
+;; (build-fmt
+;;  (:clip :size 1 :up "┌" :down "└" :vertical "│" :horizontal "─" :side :left)
+;;  (:counter :size 4 :brace-left "" :brace-right ">")
+;;  (:prefix :size 16 :brace-left "" :brace-right "")
+;;  (:msg :size 60 :brace-left "" :brace-right "")
+;;  (:order :clip :counter :prefix :msg))
+(defun build-fmt (&rest clauses)
+  (labels ((%parse-fmt-clauses (clauses)
+             (mappend (lambda (clause)
+                       (let ((type (first clause))
+                             (initargs (rest clause)))
+                         (if (eq type :order)
+                             `(:order ',initargs)
+                             (apply #'make-instance (intern (format nil "FMT-~A" (symbol-name type)) :df-cl-utils) initargs))))
+                      clauses)))
+    (apply #'make-instance 'fmt (%parse-fmt-clauses clauses))))
+
+;; options
+
+(defclass* options ()
+  (out t) ; output stream
+  (w-delim " ") ; thing that would delimit tokens
+  (nl-command :nl)
+  (nl-before-line-delim t) ; newline before line delimiter
+  (nl-after-line-delim t) ; newline after line delimiter
+  (delim-command :d[?]) ; ? is where the delimiter 
+  (delim-width 60)
+  (macros nil)) ; (:-> "arrow, lol" :<> "туди-сюди")
+
+(defun build-options (&rest clauses)
+  (apply #'make-instance 'options clauses))
+
+;; printing message
 
 (defmethod prepare-clip ((clip fmt-clip) place)
   (with-slots (up down vertical horizontal size side brace-left brace-right) clip
@@ -101,44 +137,24 @@ all the result list to a single list. FUNCTION must return a list."
   (with-slots (size brace-left brace-right) prefix
     (format* (apply-braces prefix str) size :truncate? t)))
 
-(defmethod prepare-msg ((msg fmt-msg) str)
-  )
+;; (defmethod print-message ((frmt fmt) (opts options) msg-lst)
+;;   (with-slots (frmt-clip frmt-counter frmt-prefix frmt-msg frmt-order) frmt
+;;     (with-slots (opts-out opts-w-delim opts-nl-before-line-delim opts-nl-after-line-delim opts-use-prefix? opts-macros) opts
+;;       (labels ((%process-element (el)
+;;                  )
+;;                (%get-prefix ()
+;;                  (let ((el0 (first msg-list)))
+;;                    (%process-element el0))))
+;;         (let* ((clip-upper-str (prepare-clip frmt-clip :upper))
+;;                (clip-middle-str (prepare-clip frmt-clip :middle))
+;;                (clip-lower-str (prepare-clip frmt-clip :lower))
+;;                (counter-str (prepare-counter frmt-counter))
+;;                (prefix-str (if opts-use-prefix? (prepare-prefix frmt-prefix (%get-prefix)) ""))))))))
 
-;; EXAMPLE:
+;; (defun dbp-extended (&key (msg-lst nil) (opts nil) (frmt nil))
+;;   (let ((opts (build-options opts))
+;;         (frmt (build-fmt fmt)))
+;;     (print-message frmt opts msg-lst)))
 
-;; (build-fmt
-;;  (:clip :size 1 :up "┌" :down "└" :vertical "│" :horizontal "─" :side :left)
-;;  (:counter :size 4 :brace-left "" :brace-right ">")
-;;  (:prefix :size 16 :brace-left "" :brace-right "")
-;;  (:msg :size 60 :brace-left "" :brace-right "")
-;;  (:order :clip :counter :prefix :msg))
-(defun build-fmt (&rest clauses)
-  (labels ((%parse-fmt-clauses (clauses)
-             (mappend (lambda (clause)
-                       (let ((type (first clause))
-                             (initargs (rest clause)))
-                         (if (eq type :order)
-                             `(:order ',initargs)
-                             (list type `(make-instance ',(intern (format nil "FMT-~A" (symbol-name type)) :df-cl-utils) ,@initargs)))))
-                      clauses)))
-    (apply #'make-instance 'fmt (%parse-fmt-clauses clauses))))
-
-;; options
-
-(defclass* options ()
-  (out t) ; output stream
-  (w-delim " ") ; thing that would delimit tokens
-  (nl-before-line-delim t) ; newline before line delimiter
-  (nl-after-line-delim t) ; newline after line delimiter
-  (macros nil))
-
-(defun build-options (&rest clauses)
-  (apply #'make-instance 'options clauses))
-
-(defun dbp-extended (&key (opts nil) (msg "") (fmt nil))
-  (let ((opts (build-options opts))
-        (fmt (build-fmt fmt)))
-    ()))
-
-(defmacro dbp ())
+;; (defmacro dbp ())
 
--- df-cl-utils.asd
+++ df-cl-utils.asd
@@ -1,8 +1,9 @@
-(defsystem :df-cl-utils
+(asdf:defsystem :df-cl-utils
   :description "Often used lisp code."
   :version "0.0.0"
   :author "Danylo Fedorov <fedorough@gmail.com>"
   :serial t
   :components ((:file "package")
                (:file "df-cl-utils")
-               (:file "debug-print")))
+               (:file "debug-print")
+               (:file "debug-print2")))
