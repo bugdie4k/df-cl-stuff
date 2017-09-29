3afd2c37b86b54664b19d8b5f14d0b6760cc8e2e
bugdie4k <fedorough@gmail.com>
2017-09-29 16:20:56 +0300

    defsyntax, fmt dumping

--- dbp.lisp
+++ dbp.lisp
@@ -1,11 +1,21 @@
 (in-package #:df-cl-utils)
 
-;; format+
+;; fmt+
 
-(defmacro format+ (fmt-or-string size &key (format-letter :A) align truncate? (stream nil))
-  `(format+-aux ,(if (and (listp fmt-or-string) (stringp (first fmt-or-string))) `(format nil ,@fmt-or-string) fmt-or-string) ,size :format-letter ,format-letter :align ,align :truncate? ,truncate? :stream ,stream))
+(defmacro fmt+ (fmt-or-string &key (size "") (format-letter :A) (align :left) (truncate? nil) (stream nil))
+  "`fmt-or-string' can be a string or a list of `fmt-string' and `fmt-arguments'
+(`fmt-string' is equivalent to `format-string' but ~ and : chars are swapped)
+"
+  `(fmt+-aux ,(if (and (listp fmt-or-string) (stringp (first fmt-or-string)))
+                  `(fmt nil ,@fmt-or-string)
+                  fmt-or-string)
+             :size ,size
+             :format-letter ,format-letter
+             :align ,align
+             :truncate? ,truncate?
+             :stream ,stream))
 
-(defun format+-aux (string size &key format-letter align truncate? stream)
+(defun fmt+-aux (string &key size format-letter align truncate? stream)
   (labels ((%wut-align ()
              (cond ((or (eq align :right) (eq align :r)) t)
                    ((or (eq align :left) (eq align :l)) nil)
@@ -19,7 +29,6 @@
 ;; vars
 
 (defvar *dbp-counter* 0)
-;; (defvar *dbp-format* nil)
 
 (defun dbp-reset ()
   (setf *dbp-counter* 0))
@@ -41,11 +50,9 @@
   (size 2)
   (up "┌")
   (down "└")
-  ;; (horizontal "─")
-  (horizontal " ")
+  (horizontal " ") ; "─"
   (vertical "│")
-  (oneline "•")
-  (side :left))
+  (oneline "•"))
 
 (defclasss fmt-counter (fmt-size-mixin fmt-braces-mixin)
   (size 6)
@@ -74,18 +81,38 @@
         (make-instance 'fmt)
         (apply #'make-instance 'fmt (%parse-fmt-clauses clauses)))))
 
+;; fmt dumping
+
+(defmethod make-load-form ((self fmt-clip) &optional environment)
+  (declare (ignore environment))
+  (make-load-form-saving-slots self))
+
+(defmethod make-load-form ((self fmt-counter) &optional environment)
+  (declare (ignore environment))
+  (make-load-form-saving-slots self))
+
+(defmethod make-load-form ((self fmt-prefix) &optional environment)
+  (declare (ignore environment))
+  (make-load-form-saving-slots self))
+
+(defmethod make-load-form ((self fmt-msg) &optional environment)
+  (declare (ignore environment))
+  (make-load-form-saving-slots self))
+
+(defmethod make-load-form ((self fmt) &optional environment)
+  (declare (ignore environment))
+  (make-load-form-saving-slots self))
+
 ;; prepare
 
 (defmethod prepare-clip ((clip fmt-clip) place)
-  (with-slots (up oneline down vertical horizontal size side brace-left brace-right) clip
+  (with-slots (up oneline down vertical horizontal size brace-left brace-right) clip
     (labels ((%build-corner (corner-char)
-               (let* ((corner-char (format+ corner-char 1 :truncate? t))
-                      (rest (format+ horizontal (1- size) :truncate? t))
-                      (left-part (if (eq side :left) corner-char rest))
-                      (right-part (if (eq side :left) rest corner-char)))
-                 (format nil "~A~A" left-part right-part)))
+               (let* ((corner-char (fmt+ corner-char :size 1 :truncate? t))
+                      (rest (fmt+ horizontal :size (1- size) :truncate? t)))
+                 (format nil "~A~A" corner-char rest)))
              (%build-mid (char)
-               (format+ char size :align side :truncate? t)))
+               (fmt+ char :size size :align :left :truncate? t)))
       (case place
         (:upper (%build-corner up))
         (:oneline (%build-mid oneline))
@@ -100,63 +127,63 @@
 
 (defmethod prepare-counter ((counter fmt-counter))
   (with-slots (size brace-left brace-right) counter
-     (format+ (apply-braces counter *dbp-counter*) size)))
+     (fmt+ (apply-braces counter *dbp-counter*) :size size)))
 
 (defmethod prepare-prefix ((prefix fmt-prefix) str)
   (if str
       (with-slots (size brace-left brace-right) prefix
-        (format+ (apply-braces prefix str) size :truncate? t))
+        (fmt+ (apply-braces prefix str) :size size :truncate? t))
       ""))
 
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
 
-  ;; syntax settings
+  ;; syntax
 
   (defparameter *syntax* (make-hash-table))
   (defparameter *settings* (make-hash-table))
 
-  (defmacro defcommands (class &rest clauses)
-    `(let ((class-ht (make-hash-table)))
-       (dolist (clause ',clauses)
-         (destructuring-bind (name command) clause
-           (setf (gethash name class-ht) command)))
-       (setf (gethash ',class *syntax*) class-ht)))
+  (defmacro defsyntax (&rest clauses)
+    `(dolist (clause ',clauses)
+      (destructuring-bind (name command) clause
+        (setf (gethash name *syntax*) command))))
 
-  (defmacro get-com (class name)
-    `(gethash ,name (gethash ',(intern (symbol-name class)) *syntax*)))
-
-  (defcommands com1
-    (:pref  "p>")
-    (:msg   "m>")
-    (:fmt-l "fl"))
-
-  (defcommands com2
-    (:lit "l")
-    (:nl  "nl")
-    (:cnl "cnl")
-    (:delim "d"))
+  (defmacro ^get-command (name)
+    `(gethash ,name *syntax*))
 
   (defmacro defsettings (&rest clauses)
     `(dolist (clause ',clauses)
        (destructuring-bind (name command default-value) clause
          (setf (gethash name *settings*) (cons command default-value)))))
 
-  (defmacro get-setcom (name)
+  (defmacro ^get-settings-command (name)
     `(car (gethash ,name *settings*)))
 
-  (defmacro get-setdefault (name)
+  (defmacro ^get-settings-default (name)
     `(cdr (gethash ,name *settings*)))
 
+  ;; syntax definition
+
+  (defsyntax
+    ;; com1
+    (:pref  "p>")
+    (:msg   "m>")
+    (:fmt-l "fl")
+    ;; com2
+    (:lit   "l")
+    (:nl    "nl")
+    (:cnl   "cnl")
+    (:delim "d"))
+
   (defsettings
-    (:fmt "?fmt" nil)
-    (:delim-len "?dl" 60)
-    (:delim-nl-b? "?d-nl-b" t)
-    (:delim-nl-a? "?d-nl-a" t)
-    (:stream "?s" t)
-    (:words-delim "?wd" " ")
-    (:reset-counter "?rsc" nil)
-    (:return "?return" nil))
+    (:fmt           "?fmt"    nil)
+    (:delim-len     "?dl"     60)
+    (:delim-nl-b?   "?d-nl-b" t)
+    (:delim-nl-a?   "?d-nl-a" t)
+    (:stream        "?s"      t)
+    (:words-delim   "?wd"     " ")
+    (:reset-counter "?rsc"    nil)
+    (:return        "?return" nil))
 
   (defun parse-dbp-clauses (clauses)
     (let ((prefix-list nil)
@@ -189,19 +216,21 @@
                        (error "~A must be a list" lst))))
                (%sym (sym)
                  (let ((name (symbol-name sym)))
-                   (cond ((string-equal name (get-com :com1 :pref)) (setf context :p))
-                         ((string-equal name (get-com :com1 :msg))  (setf context :msg))
-                         ((string-equal name (get-com :com1 :fmt-l)) (%fmt-l))
-                         ;; settings
-                         ((string-equal name (get-setcom :fmt))           (%fmt))
-                         ((string-equal name (get-setcom :words-delim))   (%set-next :words-delim))
-                         ((string-equal name (get-setcom :delim-len))     (%set-next :delim-len))
-                         ((string-equal name (get-setcom :delim-nl-b?))   (%set-next :delim-nl-b?))
-                         ((string-equal name (get-setcom :delim-nl-a?))   (%set-next :delim-nl-a?))
-                         ((string-equal name (get-setcom :stream))        (%set-next :stream))
-                         ((string-equal name (get-setcom :reset-counter)) (%set :reset-counter t))
-                         ((string-equal name (get-setcom :return))        (%set-next :return))
-                         (t (%context-dep-push sym)))))
+                   (cond
+                     ;; com1
+                     ((string-equal name (^get-command :pref)) (setf context :p))
+                     ((string-equal name (^get-command :msg))  (setf context :msg))
+                     ((string-equal name (^get-command :fmt-l)) (%fmt-l))
+                     ;; settings
+                     ((string-equal name (^get-settings-command :fmt))           (%fmt))
+                     ((string-equal name (^get-settings-command :words-delim))   (%set-next :words-delim))
+                     ((string-equal name (^get-settings-command :delim-len))     (%set-next :delim-len))
+                     ((string-equal name (^get-settings-command :delim-nl-b?))   (%set-next :delim-nl-b?))
+                     ((string-equal name (^get-settings-command :delim-nl-a?))   (%set-next :delim-nl-a?))
+                     ((string-equal name (^get-settings-command :stream))        (%set-next :stream))
+                     ((string-equal name (^get-settings-command :reset-counter)) (%set :reset-counter t))
+                     ((string-equal name (^get-settings-command :return))        (%set-next :return))
+                     (t (%context-dep-push sym)))))
                (%parse ()
                  (let ((el1 (pop clauses)))
                    (cond
@@ -216,7 +245,7 @@
                 fmt-init-list
                 settings-list)))))
 
-(defmacro dbp (&body clauses)
+(defun build-print-dbp-message-call (clauses)
   (multiple-value-bind (prefix-list msg-list frmt-list settings-list)
       (parse-dbp-clauses clauses)
     ;; (fmt t ":A:%:A:%:A:%:A:%" prefix-list msg-list frmt-list settings-list)
@@ -231,14 +260,14 @@
                    (if (and (consp res)
                             (eq (cdr res) t))
                        (car res)
-                       (get-setdefault key))))
+                       (^get-settings-default key))))
                (%nl-or-delim? (sym)
                  (or (%nl? sym)
                      (and (symbolp sym)
                           (%delim? (symbol-name sym)))))
                (%nl? (sym)
                  (and (symbolp sym)
-                      (string-equal (symbol-name sym) (get-com :com2 :nl))))
+                      (string-equal (symbol-name sym) (^get-command :nl))))
                (%write-by-context (str &optional (use-w-delim? t))
                  (let ((format-args `("~A~A" ,str ,(if (and use-w-delim?
                                                             (not (%nl-or-delim? use-w-delim?)))
@@ -267,24 +296,24 @@
                                           ""))))
                    (format nil "~A~A~A"
                            nl-bef
-                           (format+ ("~A" (with-output-to-string (s)
+                           (fmt+ (":A" (with-output-to-string (s)
                                             (loop for i from 0 to (1+ (/ len (length delim-el))) do
                                               (format s "~A" delim-el))))
-                                    len :truncate? t)
+                                    :size len :truncate? t)
                            nl-after)))
                (%delim? (name)
-                 (let* ((d (get-com :com2 :delim))
+                 (let* ((d (^get-command :delim))
                         (dlen (length d)))
                    (and (> (length name) dlen)
                         (string-equal (subseq name 0 dlen) d))))
                (%delim (prev name next)
-                 (%write-by-context (%construct-delim (subseq name (length (get-com :com2 :delim))) prev next) nil))
+                 (%write-by-context (%construct-delim (subseq name (length (^get-command :delim))) prev next) nil))
                (%sym (prev sym next)
                  (let ((name (symbol-name sym)))
-                   (cond ((string-equal name (get-com :com2 :nl)) (%write-by-context "~%" nil))
-                         ((string-equal name (get-com :com2 :cnl)) (%write-by-context "~&" nil))
+                   (cond ((string-equal name (^get-command :nl)) (%write-by-context "~%" nil))
+                         ((string-equal name (^get-command :cnl)) (%write-by-context "~&" nil))
                          ((%delim? name) (%delim prev name next))
-                         ((string-equal name (get-com :com2 :lit)) (setf lit? t))
+                         ((string-equal name (^get-command :lit)) (setf lit? t))
                          (t (%write/add-arg "~A" sym next)))))
                (%process-element (prev el next)
                  (if lit?
@@ -306,13 +335,19 @@
                      (loop for (a b) on msg-list do (%process-element prev a b) (setf prev a))
                      `(format nil ,(get-output-stream-string msg-format-str) ,@msg-format-args)))))
         `(progn ,(when (%getsetting :reset-counter) (dbp-reset))
-                (print-message (build-fmt ,frmt-list)
+                (print-dbp-message ,(build-fmt frmt-list)
                                :prefix-str ,(%get-prefix-format-call)
                                :msg-str    ,(%get-msg-format-call)
                                :return     ,(%getsetting :return)
                                :stream     ,(%getsetting :stream)))))))
 
-(defmethod print-message ((frmt fmt) &key prefix-str msg-str return stream)
+(defmacro dbp (&body clauses)
+  (build-print-dbp-message-call clauses))
+
+(defgeneric print-dbp-message (frmt &key prefix-str msg-str return stream)
+  (:documentation "Print debug message"))
+
+(defmethod print-dbp-message ((frmt fmt) &key prefix-str msg-str return stream)
   (destructuring-bind (up-clip oneline-clip mid-clip down-clip) (prepare-clips (clip frmt))
     (let ((prefix-str (prepare-prefix (prefix frmt) prefix-str))
           (counter-str (prog1 (prepare-counter (counter frmt)) (incf *dbp-counter*)))
@@ -339,4 +374,4 @@
         (format stream "~%")
         return))))
 
-;; (df:dbp :m> d__________________________________x d_________________________________x d________________________________x d_______________________________x d______________________________x d_____________________________x d____________________________x d___________________________x d__________________________x d_________________________x d________________________x d_______________________x d______________________x d_____________________x d____________________x d___________________x d__________________x d_________________x d________________x d_______________x d______________x d_____________x d____________x d___________x d__________x d_________x d________x d_______x d______x d_____x d____x d___x d__x d_x dx d0 d8 ?rsc ?return 123 ?rsc)
+;; (df:dbp :m> d_ d___________________________________________________________x d__________________________________________________________x d_________________________________________________________x d________________________________________________________x d_______________________________________________________x d______________________________________________________x d_____________________________________________________x d____________________________________________________x d___________________________________________________x d__________________________________________________x d_________________________________________________x d________________________________________________x d_______________________________________________x d______________________________________________x d_____________________________________________x d____________________________________________x d___________________________________________x d__________________________________________x d_________________________________________x d________________________________________x d_______________________________________x d______________________________________x d_____________________________________x d____________________________________x d___________________________________x d__________________________________x d_________________________________x d________________________________x d_______________________________x d______________________________x d_____________________________x d____________________________x d___________________________x d__________________________x d_________________________x d________________________x d_______________________x d______________________x d_____________________x d____________________x d___________________x d__________________x d_________________x d________________x d_______________x d______________x d_____________x d____________x d___________x d__________x d_________x d________x d_______x d______x d_____x d____x d___x d__x d_x dx d0 d8 ?rsc ?return 123 ?rsc)
